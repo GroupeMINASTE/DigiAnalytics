6b3d64de4f40423021e3a953582a2e492d1baac0
Nathan Fallet <contact@nathanfallet.me>
2021-05-15 14:03:20 +0200

    Bump APIRequest version number

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "APIRequest", url: "https://github.com/GroupeMINASTE/APIRequest.swift", from: "1.3.2"),
+        .package(name: "APIRequest", url: "https://github.com/GroupeMINASTE/APIRequest.swift", from: "1.3.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
